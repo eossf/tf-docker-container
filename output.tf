753c3d262ae2d25c888e739b7dced87b9d883c8d
metairie <stephane.metairie@gmail.com>
2021-09-12 11:55:52 +0200

    add debug info

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 output "container_access" {
   value = { for x in docker_container.this[*] : x.name => join(":", x.ip_address[*], x.ports[*]["external"]) }
 }
+output "debug" {
+  value = "debug"
+}
